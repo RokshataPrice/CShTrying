7cb13db52dd48e8b66db327d3f807b39619f2086
RokshataPrice <rokshataprice@gmail.com>
2019-01-10 20:51:21 +0300

    Короч, на русском. Перенесла анограммы в класс Other.

--- Program.cs
+++ Program.cs
@@ -8,46 +8,19 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static private List<string> _anogramm(List<char> chrs, int n)
-        {
-            List<string> ls = new List<string>();
-            if (n == 0 || chrs.Count == 0) ls.Add("");
-            else
-            {
-                int count = chrs.Count; 
-                for (int i = 0; i < count; i++)
-                {
-                    char chr = chrs[0];
-                    chrs.RemoveAt(0);
-                    List<string> temp = _anogramm(chrs, n - 1);
-                    for (int k = 0; k < temp.Count; k++)
-                        ls.Add(chr.ToString() + temp[k]);
-                    chrs.Add(chr);
-                }
-            }
-            return ls;
-        }
-
-        static public List<string> AnogramN(int n, List<char> chrs)
-        { 
-            List<string> strs = new List<string>();
-            strs = _anogramm(chrs, n);
-            strs = strs.Distinct().ToList<string>();
-            strs.Sort();
-            return strs;
-        }
-
-        static public string StringListToString(List<string> strs)
-        {
-            string s = "";
-            for (int i = 0; i < strs.Count; i++)
-                s += strs[i] + "\n";
-            return s;
-        }
-
         static void Main(string[] args)
         {
-            
+            List<char> chrs = new List<char>();
+            chrs.Add('в');
+            chrs.Add('и');
+            chrs.Add('к');
+            chrs.Add('л');
+            chrs.Add('м');
+            chrs.Add('о');
+            chrs.Add('о');
+            chrs.Add('о');
+            chrs.Add('т');
+            Console.WriteLine(Other.StringListToString(Other.AnogramN(4, chrs)));
             Console.ReadKey();
         }
     }
